a568b7fb7db60be266577cc5dd6065a9832dc98d
Ilham Firdausi Putra <ilhamfputra31@gmail.com>
2019-02-20 02:17:57 +0700

    add auth layout

--- android/app/src/main/java/com/chatman/MainActivity.java
+++ android/app/src/main/java/com/chatman/MainActivity.java
@@ -48,7 +48,8 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+//            super.onBackPressed();
+            this.moveTaskToBack(true);
         }
     }
 
