bee6f3f414df91643f4bbd11871e8911931cb1e5
yusufrahmatp <yusufrahmatp@gmail.com>
2019-02-25 15:14:42 +0700

    Add email feature, make command case-insensitive

--- android/app/src/main/java/com/chatman/helper/BotMessageHelper.java
+++ android/app/src/main/java/com/chatman/helper/BotMessageHelper.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
@@ -23,11 +24,11 @@ public class BotMessageHelper {
     private static final String BOT_TOKEN = "BOT_TOKEN";
 
     public static void respondBot(Context context, String message) {
-        if (message.equals("compass")) {
+        if (message.toLowerCase().equals("compass")) {
             Intent intent = new Intent(context, CompassActivity.class);
             context.startActivity(intent);
         }
-        else if (message.equals("weather today")) {
+        else if (message.toLowerCase().equals("weather today")) {
             GpsHelper gps = new GpsHelper(context);
             if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions((Activity) context, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
@@ -36,6 +37,17 @@ public class BotMessageHelper {
             else {
                 new WeatherService(context).execute(gps.getLocation());
             }
+        }
+        else if (message.split(" ", 2)[0].toLowerCase().equals("email")) {
+            String arr[] = message.split(" ", 3);
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+            intent.putExtra(Intent.EXTRA_EMAIL, arr[1]);
+            intent.putExtra(Intent.EXTRA_SUBJECT, arr[2].split(";", 2)[0]);
+            intent.putExtra(Intent.EXTRA_TEXT, arr[2].split(";", 2)[1]);
+            context.startActivity(intent);
+
+            BotMessageHelper.sendBotMessage(context, "Email draft sent to app");
         }
         else sendBotMessage(context, "Halo! Selamat datang di ChatMan");
     }
